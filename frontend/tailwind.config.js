d6e4dcf4ec91eb7da5e3c77f4e7d29ba3bac8a98
Rafael Carvalho <rafael_hosanah5@hotmail.com>
2025-08-06 16:50:43 -0300

    feat(frontend): define primary color palette

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: '#eff6ff',
+          100: '#dbeafe',
+          200: '#bfdbfe',
+          300: '#93c5fd',
+          400: '#60a5fa',
+          500: '#3b82f6',
+          600: '#2563eb',
+        },
+      },
+    },
   },
   plugins: [],
 };
