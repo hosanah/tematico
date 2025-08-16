8545e2bd06eacea57c14eb8ec29bb6b37690a86f
Rafael Carvalho <rafael_hosanah5@hotmail.com>
2025-08-16 11:48:03 -0300

    Handle new backend error response format

--- frontend/src/app/interceptors/error-interceptor.ts
+++ frontend/src/app/interceptors/error-interceptor.ts
@@ -2,13 +2,14 @@ import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { MessageService } from 'primeng/api';
 import { catchError, throwError } from 'rxjs';
+import { extractErrorMessage } from '../utils';
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
   const messageService = inject(MessageService);
 
   return next(req).pipe(
     catchError((err: HttpErrorResponse) => {
-      const detail = err.error?.error || err.error?.message || 'Erro desconhecido';
+      const detail = extractErrorMessage(err);
       messageService.add({ severity: 'error', summary: 'Erro', detail });
       return throwError(() => err);
     })
